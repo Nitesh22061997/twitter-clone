a7f559adc9e64b95b471e918e685dc6730d00b71
nitesh22061997@gmail.com <163007323+Nitesh22061997@users.noreply.github.com>
2024-06-09 17:41:49 +0530

    2nd - auth controller

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,5 @@
 import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
-import User from "../models/user.model.js";
+import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 // import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/authRoutes.js";
@@ -23,11 +23,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // const __dirname = path.resolve();
 
+app.use(express.json())
 // app.use(express.json({ limit: "5mb" })); // to parse req.body
 // // limit shouldn't be too high to prevent DOS
-// app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 // app.use("/api/users", userRoutes);
