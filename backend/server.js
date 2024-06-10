78448ee1873de99946ac154aba1523d9e11e78a8
nitesh22061997@gmail.com <163007323+Nitesh22061997@users.noreply.github.com>
2024-06-10 11:26:43 +0530

    3rd - User routes  and controllers added

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-// import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/authRoutes.js";
-// import userRoutes from "./routes/user.route.js";
+import userRoutes from "./routes/userRoutes.js";
 // import postRoutes from "./routes/post.route.js";
 // import notificationRoutes from "./routes/notification.route.js";
 
@@ -13,11 +13,11 @@ import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
-// cloudinary.config({
-//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true })); // to parse form data(urlencode
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);
 // app.use("/api/posts", postRoutes);
 // app.use("/api/notifications", notificationRoutes);
 
