adc4467743341d6c3dbb2bbc2ea57fcb2e20047e
nitesh22061997@gmail.com <163007323+Nitesh22061997@users.noreply.github.com>
2024-06-11 10:51:18 +0530

    7th -  Auth frontend completed

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -21,7 +21,7 @@ const ProfilePage = () => {
     const profileImgRef = useRef(null);
 
     const isLoading = false;
-    const isMyProfile = false;
+    const isMyProfile = true;
 
     const user = {
         _id: "1",
